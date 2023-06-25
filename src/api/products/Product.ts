f2b9bb6c6febd45005bf31de9a701c2af399a638
lee-treehouse <leentaylor@gmail.com>
2023-06-25 15:26:04 +1000

    Revert "add code to create product based off create article code as template (will be rolled back for TDD approach)"
    This reverts commit 8f30102255832cbe5e1937cca39ed4fbb4a690a6.

--- src/api/products/Product.ts
+++
@@ -1,8 +0,0 @@
-export interface Product {
-  id: string;
-  title: string;
-  description: string;
-  createdAt: Date;
-}
-
-export interface NewProduct extends Omit<Product, "id" | "createdAt"> {}
--- src/api/products/ProductsRepository.ts
+++
@@ -1,5 +0,0 @@
-import { Product, NewProduct } from "./Product";
-
-export interface ProductsRepository {
-  create(product: NewProduct): Promise<Product>;
-}
