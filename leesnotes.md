46db7d312c5dd07c868b2bb2de1b839c0d0f318b
lee-treehouse <leentaylor@gmail.com>
2023-06-25 16:20:58 +1000

    add failing test to check values in dynamodb when using post product endpoint

---
+++ leesnotes.md
@@ -0,0 +1,2 @@
+// to use my leedoeslabs profile with the aws cli by default
+export AWS_DEFAULT_PROFILE=leedoeslabs
--- test/api/product.spec.ts
+++ test/api/product.spec.ts
@@ -1,5 +1,6 @@
 import { v4 } from "uuid";
 import { request } from "../helpers/app";
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
 
 describe("Products", () => {
   describe("POST /product", () => {
@@ -7,7 +8,7 @@ describe("Products", () => {
       const requestBody = {
         product: {
           name: `product-name-${v4()}`,
-          description: `product-descriptiong-${v4()}`,
+          description: `product-description-${v4()}`,
           price: Math.random() * 50,
         },
       };
@@ -26,6 +27,37 @@ describe("Products", () => {
       expect(typeof response.body.product.id).toEqual("string");
       expect(new Date().getTime() - new Date(response.body.product.createdAt).getTime()).toBeLessThan(1000);
       expect(response.statusCode).toEqual(201);
+    });
+
+    it("stores product in database", async () => {
+      const product = {
+        name: `product-name-${v4()}`,
+        description: `product-description-${v4()}`,
+        price: Math.random() * 50,
+      };
+
+      const requestBody = {
+        product,
+      };
+
+      const response = await request.post("/product").send(requestBody);
+      const expectedProduct = response.body.product;
+
+      const client = new DynamoDBClient({});
+      const output = await client.send(
+        new GetItemCommand({
+          TableName: "Products",
+          Key: { ProductID: { S: expectedProduct.id } },
+        }),
+      );
+
+      expect(output.Item).not.toBeUndefined();
+      const item = output.Item!;
+      expect(item["ProductID"].S).toEqual(expectedProduct.id);
+      expect(item["Name"].S).toEqual(expectedProduct.name);
+      expect(item["Description"].S).toEqual(expectedProduct.description);
+      expect(item["Price"].N).toEqual(String(expectedProduct.price));
+      expect(item["CreatedAt"].N).toEqual(String(expectedProduct.createdAt));
     });
   });
 });
